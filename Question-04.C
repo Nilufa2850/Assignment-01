ea2cca7307d48954479ebfefdef9e9b8a18d0712
Nilufa Yasmin <116085061+MissNilufaYasmin@users.noreply.github.com>
2023-06-20 20:02:28 +0530

    Question-04.C

--- Question-04.C
+++ Question-04.C
@@ -1,14 +1,16 @@
 /*WAP to find the area of the circle. Take radius of circle from user as input and print the
-result in below given format.*/
+result in below given format.
+Expected output format – “Area of circle is A having the radius R”. Replace A with area
+& R with radius.*/
 
 #include<stdio.h>
 int main()
 {
-    float radius , area ;
+    float R , A ;
     printf("Enter the radius of circle : ");
-    scanf("%f",&radius);
-    area = 3.14*radius*radius;
-    printf("Area of the circle is : %f",area);
+    scanf("%f",&R);
+    A = 3.14*R*R;
+    printf("Area of circle is A=%f having the radius R=%f",A,R);
 
     return 0;
 }
