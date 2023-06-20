2ada45e1310a43857e592098e3900d85bf7ca66e
Nilufa Yasmin <116085061+MissNilufaYasmin@users.noreply.github.com>
2023-06-20 19:50:08 +0530

    Question-02.C

---
+++ Question-02.C
@@ -0,0 +1,9 @@
+//Write a program to print Hello in the first line and Students in the second line.//
+
+#include<stdio.h>
+int main()
+{
+    printf("Hello\n");
+    printf("Students");
+    return 0;
+}
